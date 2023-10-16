1174dd7f066125058dfc4fe1639c89b7b90003f4
Serg Skol <sps676@gmail.com>
2023-10-16 09:37:09 +0200

    Registration of New Users

--- server.js
+++ server.js
@@ -31,13 +31,12 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    //Change the response to render the Pug template
     res.render('index', {
       title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true
+      message: 'Please log in',
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -54,12 +53,43 @@ myDB(async client => {
     res.redirect('/');
   });
 
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
   app.use((req, res, next) => {
     res.status(404)
       .type('text')
       .send('Not Found');
   });
-  
+
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
